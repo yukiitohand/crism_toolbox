d852cb3972347709de101b9cf5911b8ca507cd7d
Yuki Itoh <yukiitohand@gmail.com>
2019-03-18 23:08:10 -0400

    setting supports mkdir

--- setting/crism_init.m
+++ setting/crism_init.m
@@ -3,6 +3,9 @@ function crism_init()
     str = fileread('crismToolbox.json');
     crism_env_vars = jsondecode(str);
     
+    check_mkdir(crism_env_vars.localCRISM_PDSrootDir);
+    check_mkdir(crism_env_vars.localCATrootDir);
+    
     crism_env_vars.url_local_root = crism_env_vars.([crism_env_vars.local_fldsys '_URL']);
     crism_env_vars.url_remote_root = crism_env_vars.([crism_env_vars.remote_fldsys '_URL']);
     
@@ -16,3 +19,28 @@ function crism_init()
     end
 
 end
+
+function [] = check_mkdir(dirpath)
+    exist_flg = exist(dirpath,'dir');
+    if exist_flg
+        %yesno = 1;
+    else
+        flg = 1;
+        while flg
+            prompt = sprintf('%s does not exist. Do you want to create?(y/n)',dirpath);
+            ow = input(prompt,'s');
+            if any(strcmpi(ow,{'y','n'}))
+                flg=0;
+            else
+                fprintf('Input %s is not valid.\n',ow);
+            end
+        end
+        if strcmpi(ow,'n')
+            fprintf('No local database will be created.\n');
+        elseif strcmpi(ow,'y')
+            fprintf('%s is created...\n',dirpath);
+        end
+    end
+end
+
+    
