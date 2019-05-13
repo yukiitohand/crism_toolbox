8b78cd8cd56038300ade5ca349d86256673124bf
Yuki Itoh <yukiitohand@gmail.com>
2019-05-13 11:32:44 -0400

    FFC loader is updated

--- util/get_scene_CRISMdata_FFC.m
+++ util/get_scene_CRISMdata_FFC.m
@@ -1,6 +1,6 @@
 function [crismdata_obj_scene] = get_scene_CRISMdata_FFC(basenames,dirpath,ffc_counter)
 % [crismdata_obj_scene] = get_scene_CRISMdata_FFC(basenames,dirpath,ffc_counter)
-% get scene CRISMdata obj for scene.
+% get scene CRISMdata obj for scene. Return empty if no data is found.
 % INPUTS
 %  basenames: basename of the CRISM data, or a cell array of it
 %  dirpath  : path to the file, can be empty
@@ -15,27 +15,26 @@ if ischar(ffc_counter)
     ffc_counter = hex2dec(ffc_counter);
 end
 
-if ischar(basenames) || isempty(basenames)
-    basename = basenames;
-elseif iscell(basenames)
-    switch ffc_counter
-        case 1
-            idx = 1;
-        case 3
-            idx = 2;
-        otherwise
-            error('Check data');
+if isempty(basenames)
+    crismdata_obj_scene = [];
+else
+    if ischar(basenames)
+        basenames = {basenames};
     end
-    basename = basenames{idx};
+
+    propList = cellfun(@(x) getProp_basenameOBSERVATION(x), basenames);
+    obs_ctrs = cellfun(@(x) hex2dec(x), {propList.obs_counter});
+    idx = find(obs_ctrs==ffc_counter);
+    
+    if isempty(idx)
+        crismdata_obj_scene = [];
+    elseif length(idx)>2
+        error('Something wrong.');
+    else
+        basename = basenames{idx};
+        crismdata_obj_scene = get_CRISMdata(basename,dirpath);
+    end
 end
 
-crismdata_obj_scene = get_CRISMdata(basename,dirpath);
-
-switch upper(crismdata_obj_scene.lbl.OBSERVATION_TYPE)
-    case 'FFC'
-        if hex2dec(crismdata_obj_scene.prop.obs_counter) ~= ffc_counter
-            error('crism_obs is something wrong');
-        end
-end
 
 end
