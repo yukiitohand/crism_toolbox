4247e5dced11eae7766dce49c3e443c85720b5bd
Yuki Itoh <yukiitohand@gmail.com>
2020-10-16 17:54:31 -0400

    a new function is added

---
+++ util/crism_get_TRRXbasename.m
@@ -0,0 +1,27 @@
+function [basename_trrx] = crism_get_TRRXbasename(TRRdata,vr)
+% [basename_trrx] = crism_get_TRRXbasename(TRRdata,vr)
+% Get the basename of the TRR* data.
+%  INPUTS
+%   TRRdata: CRISMdata object
+%   vr: version of the TRR*data. one character in 0-9A-Z
+%  OUTPUTS
+%   basename_trrx: char, basename of the trrx
+% 
+
+if ~isa(TRRdata,'CRISMdata')
+    error('Input 1 must be an instance of CRISMdata');
+end
+if isnumeric(vr)
+   if ~isinteger(vr) || vr<0 || vr>9
+       error('Input 2 must be one digit number or a one character (A-Z)');
+   end
+elseif ischar(vr)
+    if isempty(regexpi(vr,'^[0-9A-Z]{1}$','once'))
+       error('Input 2 must be one digit number or a one character (A-Z)');
+    end
+end
+    
+propTRRX = TRRdata.prop;
+propTRRX.version = vr;
+basename_trrx = get_basenameOBS_fromProp(propTRRX);
+end
--- util/get_TRRXdata.m
+++ util/get_TRRXdata.m
@@ -1,10 +1,6 @@
 function [TRRXIFdata] = get_TRRXdata(TRRdata,vr,dir_trrx)
 
-product_type = 'TRR';
-propXIF = getProp_basenameOBSERVATION(TRRdata.basename);
-propXIF.product_type = product_type;
-propXIF.version = vr;
-bnameXIF = get_basenameOBS_fromProp(propXIF);
-TRRXIFdata = CRISMdata(bnameXIF,dir_trrx);
+[basename_trrx] = crism_get_TRRXbasename(TRRdata,vr);
+TRRXIFdata = CRISMdata(basename_trrx,dir_trrx);
 
 end
