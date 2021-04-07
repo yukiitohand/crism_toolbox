5c20e6d22c04cde0a7c7be9063559ff01944c5c8
Yuki Itoh <yukiitohand@gmail.com>
2021-04-07 16:32:08 -0400

    angularx and angularxy are now supported

--- spice/cahv/crism_get_pmc_pxlctrs.m
+++ spice/cahv/crism_get_pmc_pxlctrs.m
@@ -1,4 +1,4 @@
-function [pmc_pxlctrs] = crism_get_pmc_pxlctrs(crism_camera_info,cahv_mdl,binx)
+function [pmc_pxlctrs] = crism_get_pmc_pxlctrs(crism_camera_info,cahv_mdl,binx,varargin)
 % [pmc_pxlctrs] = crism_get_pmc_pxlctrs(crism_camera_info,cahv_mdl,binx)
 % Get (P-C) pointing vector for the CRISM pixel centers
 % INPUTS
@@ -9,9 +9,29 @@ function [pmc_pxlctrs] = crism_get_pmc_pxlctrs(crism_camera_info,cahv_mdl,binx)
 %  pmc_pxlctrs: [3 x (Ncrism)]
 %     (P-C) associated with the pixel centers. Ncrism is the number of 
 %     CRISM cross-track pixels. (640 with no binning)
-%
+% OPTIONAL Parameters
+%   'PROJ_MODE': projection mode, either of {'ANGULARX','PLANAR','ANGULARXY'}
+%     'ANGULARX': angular projection only in the cross track direction
+%     'ANGULARXY': angular projection both in the cross and along track
+%     directions.
+%     'PLANAR': planar projection using CAHV model.
+%     (default) 'ANGULARX'
 % 
 % Copyright (C) 2021 Yuki Itoh <yukiitohand@gmail.com>
+
+proj_mode = 'ANGULARX'; % {'ANGULARX','ANGULARXY','PLANE'}
+if (rem(length(varargin),2)==1)
+    error('Optional parameters should always go by pairs');
+else
+    for i=1:2:(length(varargin)-1)
+        switch upper(varargin{i})
+            case 'PROJ_MODE'
+                proj_mode = varargin{i+1};
+            otherwise
+                error('Unrecognized option: %s', varargin{i});   
+        end
+    end
+end
 
 x = 0:639;
 
@@ -19,8 +39,18 @@ h = fspecial('average',[1,binx]); % convolution vector
 xbinx = conv(x,h,'valid'); % take convolution and 
 xbinx = xbinx(1:binx:end);
 
-[pmc_pxlctrs] = crism_cahv_get_pmc([xbinx;zeros(size(xbinx))], ...
-    crism_camera_info,cahv_mdl);
+switch upper(proj_mode)
+    case 'ANGULARX'
+        [pmc_pxlctrs] = crism_cahv_get_pmc_angularx([xbinx;zeros(size(xbinx))], ...
+            crism_camera_info,cahv_mdl);
+    case 'ANGULARXY'
+        [pmc_pxlctrs] = crism_cahv_get_pmc_angularxy([xbinx;zeros(size(xbinx))], ...
+            crism_camera_info,cahv_mdl);
+    case 'PLANAR'
+        [pmc_pxlctrs] = cahv_mdl.get_p_minus_c_from_xy([xbinx;zeros(size(xbinx))]);
+    otherwise
+        error('Undefined PROJ_MODE %s',proj_mode);
+end
 
 
 % a0_ref = crism_camera_info.ref_camera_coeff(2);
