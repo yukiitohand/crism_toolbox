3716d4c99284a01f7c57a08cf5aa8f0cd45633c0
Yuki Itoh <yukiitohand@gmail.com>
2021-10-06 16:48:34 -0400

    hard coded path is fixed

--- spice/util/crism_get_spice_dirpath.m
+++ spice/util/crism_get_spice_dirpath.m
@@ -1,7 +1,12 @@
 function [dirpath] = crism_get_spice_dirpath(kernel_name,varargin)
 
+global naif_archive_env_vars
+global crism_env_vars
 
-pdir = '/Volumes/LaCie5TB/data/naif.jpl.nasa.gov/pub/naif/MRO/kernels/';
+% pdir = '/Volumes/LaCie5TB/data/naif.jpl.nasa.gov/pub/naif/MRO/kernels/';
+pdir = joinPath(naif_archive_env_vars.local_naif_archive_rootDir, ...
+    naif_archive_env_vars.naif_archive_root_URL, ...
+    crism_env_vars.NAIF_MROSPICE_subdir);
 if (rem(length(varargin),2)==1)
     error('Optional parameters should always go by pairs');
 else
