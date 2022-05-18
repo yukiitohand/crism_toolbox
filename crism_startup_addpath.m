2ccc51927b8f5bf58ab32c7ab8f5a03c66e838ee
Yuki Itoh <yukiitohand@gmail.com>
2022-05-17 23:34:07 -0400

    startup script takes the version into account

--- crism_startup_addpath.m
+++ crism_startup_addpath.m
@@ -1,74 +1,39 @@
 function [] = crism_startup_addpath()
-%-------------------------------------------------------------------------%
-% % Automatically find the path to toolboxes
+%% Automatically find the path to toolboxes
 fpath_self = mfilename('fullpath');
 [dirpath_self,filename] = fileparts(fpath_self);
-
-mtch = regexpi(dirpath_self,'(?<parent_dirpath>.*)/crism_toolbox[/]{0,1}','names');
+path_ptrn = '(?<parent_dirpath>.*)/(?<toolbox_dirname>[^/]+[/]{0,1})';
+mtch = regexpi(dirpath_self,path_ptrn,'names');
 toolbox_root_dir = mtch.parent_dirpath;
+crism_toolbox_dirname  = mtch.toolbox_dirname;
 
-
-%-------------------------------------------------------------------------%
-% name of the directory of each toolbox
-base_toolbox_dirname          = 'base';
-envi_toolbox_dirname          = 'envi';
-pds3_toolbox_dirname          = 'pds3_toolbox';
-crism_toolbox_dirname         = 'crism_toolbox';
-spice_dirname                 = 'spice';
-
-%-------------------------------------------------------------------------%
+%% find dependent toolboxes
+dList = dir(toolbox_root_dir);
+error_if_not_unique = true;
 pathCell = strsplit(path, pathsep);
 
-%-------------------------------------------------------------------------%
-%%
 % base toolbox
-base_toolbox_dir = [toolbox_root_dir '/' base_toolbox_dirname '/'];
-addpath(base_toolbox_dir);
-% joinPath in base toolbox will be used in the following. "base" toolbox
-% need to be loaded first. base/joinPath.m automatically determine the
-% presence of trailing slash, so you do not need to worry it.
-if exist(base_toolbox_dir,'dir')
-    if ~check_path_exist(base_toolbox_dir, pathCell)
-        addpath(base_toolbox_dir);
-    end
-else
-    warning([ ...
-        'base toolbox is not detected. Download from' '\n' ...
-        '   https://github.com/yukiitohand/base/'
-        ]);
+[base_toolbox_dir,base_toolbox_dirname] = get_toolbox_dirname(dList, ...
+    'base',error_if_not_unique);
+if ~check_path_exist(base_toolbox_dir, pathCell)
+    addpath(base_toolbox_dir);
 end
 
-%%
 % envi toolbox
-envi_toolbox_dir = joinPath(toolbox_root_dir, envi_toolbox_dirname);
-
-if exist(envi_toolbox_dir,'dir')
-    if ~check_path_exist(envi_toolbox_dir, pathCell)
-        run(joinPath(envi_toolbox_dir,'envi_startup_addpath'));
-    end
-else
-    warning([ ...
-        'envi toolbox is not detected. Download from' '\n' ...
-        '   https://github.com/yukiitohand/envi/'
-        ]);
+[envi_toolbox_dir,envi_toolbox_dirname] = get_toolbox_dirname(dList, ...
+    'envi',error_if_not_unique);
+if ~check_path_exist(envi_toolbox_dir, pathCell)
+    run(joinPath(envi_toolbox_dir,'envi_startup_addpath'));
 end
 
-%%
-pds3_toolbox_dir = joinPath(toolbox_root_dir, pds3_toolbox_dirname);
-
-if exist(pds3_toolbox_dir,'dir')
-    if ~check_path_exist(pds3_toolbox_dir, pathCell)
-        run(joinPath(pds3_toolbox_dir,'pds3_startup_addpath'));
-    end
-else
-    warning([ ...
-        'pds3_toolbox is not detected. Download from' '\n' ...
-        '   https://github.com/yukiitohand/pds3_toolbox/'
-        ]);
+% pds3_toolbox
+[pds3_toolbox_dir,pds3_toolbox_dirname] = get_toolbox_dirname(dList, ...
+    'pds3_toolbox',error_if_not_unique);
+if ~check_path_exist(pds3_toolbox_dir, pathCell)
+    run(joinPath(pds3_toolbox_dir,'pds3_startup_addpath'));
 end
 
-%%
-% crism_toolbox
+%% crism_toolbox
 crism_toolbox_dir = joinPath(toolbox_root_dir, crism_toolbox_dirname);
 if ~check_path_exist(crism_toolbox_dir, pathCell)
     addpath( ...
@@ -124,6 +89,7 @@ if ~check_path_exist(crism_toolbox_dir, pathCell)
 end
 
 %% SPICE/MICE
+spice_dirname = 'spice';
 spice_toolbox_dir = joinPath(toolbox_root_dir,spice_dirname);
 spice_mice_toolbox_dir = joinPath(spice_toolbox_dir,'mice/');
 if exist(spice_mice_toolbox_dir,'dir')
@@ -132,10 +98,6 @@ if exist(spice_mice_toolbox_dir,'dir')
         joinPath(spice_mice_toolbox_dir,'src/mice/')  ...
     );
 else
-    addpath( ...
-        joinPath(spice_mice_toolbox_dir,'lib/')     , ...
-        joinPath(spice_mice_toolbox_dir,'src/mice/')  ...
-    );
     fwrite(1, ...
         sprintf([ ...
             'Message:' '\n'...
@@ -160,5 +122,37 @@ function [onPath] = check_path_exist(dirpath, pathCell)
       onPath = any(strcmpi(dirpath, pathCell));
     else
       onPath = any(strcmp(dirpath, pathCell));
+    end
+end
+
+function [toolbox_dirpath,toolbox_dirname] = get_toolbox_dirname(dList,toolbox_dirname_wover,error_if_not_unique)
+    dirname_ptrn = sprintf('(?<toolbox_dirname>%s(-[\\d\\.]+){0,1}[/]{0,1})',toolbox_dirname_wover);
+    mtch_toolbox_dirname = regexpi({dList.name},dirname_ptrn,'names');
+    mtchidx = find(not(cellfun('isempty',mtch_toolbox_dirname)));
+    toolbox_root_dir = dList(1).folder;
+    if length(mtchidx)==1
+        toolbox_dirname = mtch_toolbox_dirname{mtchidx}.toolbox_dirname;
+        toolbox_dirpath = [toolbox_root_dir, '/', toolbox_dirname];
+    elseif isempty(mtchidx)
+        toolbox_dirname = [];
+        toolbox_dirpath = [];
+        if error_if_not_unique
+            
+            error(['Cannot find %s toolbox in\n %s', ...
+                   'Download from\n', ...
+                   '   https://github.com/yukiitohand/%s/', ...
+                  ], ...
+                toolbox_dirname_wover,toolbox_root_dir,toolbox_dirname_wover);
+        end
+    else % length(mtchidx)>1
+        toolbox_dirname = {cat(2,mtch_toolbox_dirname{mtchidx}).toolbox_dirname};
+        toolbox_dirpath = cellfun(@(x) strjoin({toolbox_root_dir,x},'/'), toolbox_dirname, ...
+            'UniformOutput',false);
+        if error_if_not_unique
+            toolbox_root_dir = dList(1).folder;
+            error('Multiple %s toolboxes %s are detected in\n %s', ...
+            toolbox_dirname_wover,strjoin(toolbox_dirname,','), toolbox_root_dir);
+        end
+        
     end
 end
