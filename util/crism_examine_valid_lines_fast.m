ee326897ff0d934eb8ce011631d237c726621ecb
Yuki Itoh <yukiitohand@gmail.com>
2022-11-15 12:59:09 -0500

    function is improved

--- util/crism_examine_valid_lines_fast.m
+++ util/crism_examine_valid_lines_fast.m
@@ -1,4 +1,4 @@
-function [valid_lines_bool] = crism_examine_valid_lines_fast(hkp_fpath)
+function [valid_lines_bool] = crism_examine_valid_lines_fast(hkp_fpath,varargin)
 % [valid_lines_bool] = crism_examine_valid_lines_fast(hkp_fpath)
 % examine valid lines from the scan motor position
 % Input parameters
@@ -10,7 +10,29 @@ function [valid_lines_bool] = crism_examine_valid_lines_fast(hkp_fpath)
 
 scan_motor_pos3(scan_motor_pos3<(-2^21-1)) = scan_motor_pos3(scan_motor_pos3<(-2^21-1)) + (2^22-1);
 scan_motor_pos1(scan_motor_pos1<(-2^21-1)) = scan_motor_pos1(scan_motor_pos1<(-2^21-1)) + (2^22-1);   
-scan_motor_diff = abs(scan_motor_pos3- scan_motor_pos1);
-valid_lines_bool = scan_motor_diff>median(scan_motor_diff)*0.6;
+scan_motor_diff = abs(scan_motor_pos3 - scan_motor_pos1);
+max_diff = max(scan_motor_diff);
+
+w = double(scan_motor_diff>0.01*max_diff);
+
+% Solve a minimization problem
+L = length(scan_motor_diff);
+x = ( (1:L) - 0.5*(1+L) ) ./ (0.5*L); x = x(:);
+
+n = 3;
+A = ones(L,n+1);
+for i=1:n
+    A(:,i+1) = legendreP(i,x);
+end
+
+Anrm = vnorms(A,1,2);
+Anrmd = A ./ Anrm;
+
+[x1] = wclad_admm_gat(Anrmd,scan_motor_diff,'W',w,'verbose','no','tol',1e-5,'maxiter',1000);
+
+r = abs(scan_motor_diff - Anrmd*x1);
+
+
+valid_lines_bool = and(r < 200,logical(w));
 
 end
